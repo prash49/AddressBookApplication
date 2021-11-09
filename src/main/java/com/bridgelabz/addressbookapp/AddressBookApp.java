f5f7990f3595faca322da01824e581991c3e63fe
Prashanth <imprash4u@gmail.com>
2021-11-09 23:32:42 +0530

    Added: Logging Levels using Lombok Library

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookApp.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApp.java
@@ -1,13 +1,26 @@
 package com.bridgelabz.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+
+@Slf4j
 public class AddressBookApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookApp.class, args);
+
+        ApplicationContext context = SpringApplication.run(AddressBookApp.class, args);
+        log.info("AddressBook App Started in {} Environment",
+                context.getEnvironment().
+
+                        getProperty("environment"));
+        log.info("AddressBook App DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
+
