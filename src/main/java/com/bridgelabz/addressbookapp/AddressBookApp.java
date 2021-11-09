730530b81263f362ddf496378b748ec668c5b881
Prashanth <imprash4u@gmail.com>
2021-11-09 21:53:38 +0530

    Added: Intialized Project Structure

---
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApp.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApp.class, args);
+    }
+
+}
